5fadb0362ff64ce74af877213385ed4132708b96
Christopher Schmidt <62810427+C-h-r-i-s-x@users.noreply.github.com>
2022-12-16 23:04:36 +0100

    Update 32_Lattice_paths_and_determinants.lean (#19)
    * Update 32_Lattice_paths_and_determinants.lean
    
    def
    directed graph
    directed walk
    
    * Update src/chapters/32_Lattice_paths_and_determinants.lean
    
    Co-authored-by: Moritz Firsching <firsching@google.com>
    
    Co-authored-by: Moritz Firsching <firsching@google.com>

--- src/chapters/32_Lattice_paths_and_determinants.lean
+++ src/chapters/32_Lattice_paths_and_determinants.lean
@@ -16,7 +16,10 @@ limitations under the License.
 Authors: Moritz Firsching, Christopher Schmidt
 -/
 import tactic
+import combinatorics.simple_graph.basic
+import combinatorics.simple_graph.subgraph
 import combinatorics.simple_graph.acyclic
+import data.list.rotate
 /-!
 # Lattice paths and determinants
 
@@ -24,6 +27,7 @@ import combinatorics.simple_graph.acyclic
 
  - missing definitions:
   - directed graphs (trivial to do)
+  - acyclic graphs
   - Path
   - weights on edge set (trivial to do)
   - definition for weight of Path
@@ -37,13 +41,48 @@ import combinatorics.simple_graph.acyclic
     - proof
   - Theorem
     - proof
+
+### Introducing missing Objects
 -/
 universe u
 
--- Copying the structure from simple_graph.
+-- Copying the structure of graph and walk from simple_graph.
 @[ext]
 structure simple_directed_graph (V : Type u) := (adj : V → V → Prop)
 
 noncomputable instance {V : Type u} [fintype V] : fintype (simple_directed_graph V) :=
 by { classical,
 exact fintype.of_injective simple_directed_graph.adj simple_directed_graph.ext,}
+
+namespace simple_directed_graph
+
+variables {V : Type u}
+variables (G : simple_directed_graph V)
+
+@[derive decidable_eq]
+inductive directed_walk : V → V → Type u
+| nil {u : V} : directed_walk u u
+| cons {u v w: V} (h : G.adj u v) (p : directed_walk v w) : directed_walk u w
+
+attribute [refl] directed_walk.nil
+
+namespace directed_walk
+/-- Pattern to get `walk.nil` with the vertex as an explicit argument. -/
+@[pattern] abbreviation nil' (u : V) : G.directed_walk u u := directed_walk.nil
+
+/-- Pattern to get `walk.cons` with the vertices as explicit arguments. -/
+@[pattern] abbreviation cons' (u v w : V) (h : G.adj u v) (p : G.directed_walk v w) :
+G.directed_walk u w := directed_walk.cons h p
+
+def support : Π {u v : V}, G.directed_walk u v → list V
+| u v nil := [u]
+| u v (cons h p) := u :: p.support
+
+/-- The `edges` of a walk is the list of edges it visits in order. -/
+def edges: Π {u v : V}, G.directed_walk u v → list (V × V) :=
+--TODO
+
+
+end directed_walk
+
+end simple_directed_graph
