29407875f279b34d0685e80fc0d8180aa751f119
Moritz Firsching <firsching@google.com>
2023-09-01 21:31:59 +0200

    update mathlib

--- lakefile.lean
+++ lakefile.lean
@@ -5,10 +5,11 @@ package formal_book {
   -- add any package configuration options here
 }
 
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
 
 @[default_target]
 lean_lib «FormalBook» {
   -- add any library configuration options here
 }
-
-require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "6c5ad83c2ca4a6e03ae9e33fa52220712b8a9faf"
