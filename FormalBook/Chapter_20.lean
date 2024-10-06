53c19c5fa426f377928044c17bb5aba643a20fe8
Moritz Firsching <firsching@google.com>
2024-10-06 12:13:38 +0200

    latex +proof ch20

--- FormalBook/Chapter_20.lean
+++ FormalBook/Chapter_20.lean
@@ -15,11 +15,14 @@ limitations under the License.
 
 Authors: Moritz Firsching
 -/
-import Mathlib.Tactic
--- import Mathlib.Analysis.InnerProductSpace.Basic
-import Mathlib.Algebra.Module.Basic
-import Mathlib.Data.Real.Basic
--- open real_inner_product_space
+import Mathlib.Analysis.InnerProductSpace.Basic
+import Mathlib.Analysis.SpecialFunctions.Pow.Real
+import Mathlib.Data.Real.StarOrdered
+
+open Real
+open RealInnerProductSpace
+open BigOperators
+open Classical
 
 /-!
 # In praise of inequalities
@@ -45,21 +48,83 @@ import Mathlib.Data.Real.Basic
 
 -- porting note: waiting for
 -- https://leanprover-community.github.io/mathlib-port-status/file/analysis/inner_product_space/basic
-/-!
+
 -- Not quite sure what we actually need here, want to have ℝ-vector space with inner product.
-variables {V : Type _}  [add_comm_group V] [module ℝ V] [inner_product_space ℝ V] [normed_space ℝ V]
+variable (V : Type*) [NormedAddCommGroup V] [InnerProductSpace ℝ V] [DecidableEq V]
 
-theorem cauchy_schwarz_inequality (a b : V) : ⟪a, b⟫^2 ≤ ‖a‖^2 * ‖b‖^2 :=
-begin
-  have h: ∀ (x : ℝ), ‖x•a + b‖^2 = x^2*‖a‖^2 + 2*x*⟪a, b⟫ + ‖b‖^2 := by
-  { cases em (a = 0),
-    { rw h,
-      intro x,
-      sorry, },
-    { cases em (∃ (l : ℝ),  b = l•a),
-      { sorry, },
-      { sorry, }, }, },
-  sorry,
-end
+theorem cauchy_schwarz_inequality (a b : V) : ⟪ a, b ⟫ ^ 2 ≤ ‖a‖ ^ 2 * ‖b‖ ^ 2 := by
+  have h: ∀ (x : ℝ), ‖x • a + b‖ ^ 2 = x ^ 2 * ‖a‖ ^ 2 + 2 * x * ⟪a, b⟫ + ‖b‖ ^ 2 := by
+    simp only [pow_two, ← (real_inner_self_eq_norm_mul_norm _)]
+    simp only [inner_add_add_self, inner_smul_right, inner_smul_left, conj_trivial,
+        add_left_inj, real_inner_comm]
+    intro x
+    ring_nf
+  by_cases ha : a = 0
+  · rw [ha]
+    simp
+  · by_cases hl : (∃ (l : ℝ),  b = l • a)
+    · obtain ⟨l, hb⟩ := hl
+      rw [hb]
+      simp only [pow_two, ← (real_inner_self_eq_norm_mul_norm _)]
+      simp only [inner_add_add_self, inner_smul_right, inner_smul_left, conj_trivial,
+        add_left_inj]
+      ring_nf
+      rfl
+    · have : ∀ (x : ℝ), 0 < ‖x • a + b‖ := by
+        intro x
+        by_contra hx
+        simp only [norm_pos_iff, ne_eq, Decidable.not_not] at hx
+        absurd hl
+        use -x
+        rw [← add_zero (-x•a), ← hx]
+        simp only [neg_smul, neg_add_cancel_left]
+      have : ∀ (x : ℝ), 0 < ‖x • a + b‖^2 := by
+        exact fun x ↦ sq_pos_of_pos (this x)
+      have : ∀ (x : ℝ), 0 <  x ^ 2 * ‖a‖ ^ 2 + 2 * x * ⟪a, b⟫ + ‖b‖ ^ 2 := by
+        convert this
+        exact (h _).symm
+      have : ∀ (x : ℝ), 0 <  ‖a‖ ^ 2 * x * x  + 2 * ⟪a, b⟫ * x + ‖b‖ ^ 2 := by
+        intro x
+        calc
+          0 <  x ^ 2 * ‖a‖ ^ 2 + 2 * x * ⟪a, b⟫ + ‖b‖ ^ 2 := this x
+          _ = ‖a‖ ^ 2 * x * x  + 2 * ⟪a, b⟫ * x + ‖b‖ ^ 2  := by ring_nf
+      have ha_sq : ‖a‖ ^ 2 ≠ 0 := by aesop
+      have := discrim_lt_zero ha_sq this
+      unfold discrim at this
+      have  : (2 * inner a b) ^ 2 < 4 * ‖a‖ ^ 2 * ‖b‖ ^ 2 := by linarith
+      linarith
 
--/
+
+theorem harmonic_geometric_arithmetic₁ (n : ℕ) (hn : 1 ≤ n)
+  (a : Finset.Icc 1 n → ℝ) (hpos : ∀ i, 0 < a i) :
+  let harmonic := n / (∑ i : Finset.Icc 1 n, 1 / (a i))
+  let geometric := (∏ i : Finset.Icc 1 n, a i) ^ ((1 : ℝ) / n)
+  let arithmetic := (∑ i : Finset.Icc 1 n, a i) / n
+  let all_equal := ∀ i : Finset.Icc 1 n, a i = a ⟨1, Finset.mem_Icc.mpr  ⟨NeZero.one_le, hn⟩⟩
+  harmonic ≤ geometric ∧ geometric ≤ arithmetic ∧
+  ((harmonic = geometric) ↔ all_equal) ∧
+  ((geometric = arithmetic) ↔ all_equal) := by sorry
+
+
+
+theorem harmonic_geometric_arithmetic₂ (n : ℕ) (hn : 1 ≤ n)
+  (a : Finset.Icc 1 n → ℝ) (hpos : ∀ i, 0 < a i) :
+  let harmonic := n / (∑ i : Finset.Icc 1 n, 1 / (a i))
+  let geometric := (∏ i : Finset.Icc 1 n, a i) ^ ((1 : ℝ) / n)
+  let arithmetic := (∑ i : Finset.Icc 1 n, a i) / n
+  let all_equal := ∀ i : Finset.Icc 1 n, a i = a ⟨1, Finset.mem_Icc.mpr  ⟨NeZero.one_le, hn⟩⟩
+  harmonic ≤ geometric ∧ geometric ≤ arithmetic ∧
+  ((harmonic = geometric) ↔ all_equal) ∧
+  ((geometric = arithmetic) ↔ all_equal) := by sorry
+
+
+theorem harmonic_geometric_arithmetic₃ (n : ℕ) (hn : 1 ≤ n)
+  (a : Finset.Icc 1 n → ℝ) (hpos : ∀ i, 0 < a i) :
+  let harmonic := n / (∑ i : Finset.Icc 1 n, 1 / (a i))
+  let geometric := (∏ i : Finset.Icc 1 n, a i) ^ ((1 : ℝ) / n)
+  let arithmetic := (∑ i : Finset.Icc 1 n, a i) / n
+  let all_equal := ∀ i : Finset.Icc 1 n, a i = a ⟨1, Finset.mem_Icc.mpr  ⟨NeZero.one_le, hn⟩⟩
+  harmonic ≤ geometric ∧ geometric ≤ arithmetic ∧
+  ((harmonic = geometric) ↔ all_equal) ∧
+  ((geometric = arithmetic) ↔ all_equal) := by sorry
+
