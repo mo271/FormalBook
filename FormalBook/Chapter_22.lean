f6a930bbde2c0f6db29ca274803aa55b12dfeb96
dhyan-aranha <dhyan.aranha@gmail.com>
2024-11-21 15:06:49 +0100

    added coloring function and a couple of basic lemmas about it

--- FormalBook/Chapter_22.lean
+++ FormalBook/Chapter_22.lean
@@ -26,13 +26,92 @@ Jonas van der Schaaf,
 Lenny Taelman,
 Dhyan Aranha.
 -/
+
 import Mathlib.Tactic
+import Mathlib.Analysis.InnerProductSpace.PiL2
+import Mathlib.RingTheory.Valuation.Basic
+open BigOperators
+
 /-!
 # One square and an odd number of triangles
+-/
 
-## TODO
+local notation "ℝ²" => EuclideanSpace ℝ (Fin 2)
+
+
+inductive Rainbow
+| Red
+| Green
+| Blue
+
+def painter (Γ₀ : Type) (locg : LinearOrderedCommGroupWithZero Γ₀) (v : Valuation ℝ Γ₀) :
+ℝ² → Rainbow
+| X => if v (X 0)  < v (1) ∧ v (X 1) < v 1 then Rainbow.Red
+  else if v (X 0) < v (X 1) ∧ v (X 1) ≥ v 1 then Rainbow.Green
+  else Rainbow.Blue
+
+lemma painted_green (Γ₀ : Type) (locg : LinearOrderedCommGroupWithZero Γ₀) (v : Valuation ℝ Γ₀)
+ (X : ℝ²) : painter Γ₀ locg v X = Rainbow.Green → v (X 1) ≥  v (1) := by
+  intro h
+  simp only [painter, Fin.isValue, map_one, ge_iff_le] at h
+  split_ifs at h with h1 h2
+  cases' h2 with p q
+  rw [v.map_one]
+  exact q
+
+lemma dist_negation_over_and (P Q : Prop): ¬(P ∧ Q) ↔ ¬P ∨ ¬Q := by
+  constructor
+  · intro h
+    by_cases hP : P
+    · right
+      intro hQ
+      apply h
+      exact ⟨hP, hQ⟩
+    · left
+      exact hP
+  · rintro (hnP | hnQ) ⟨hP, hQ⟩
+    · contradiction
+    · apply hnQ; exact hQ
+
+
+
+lemma painted_blue (Γ₀ : Type) (locg : LinearOrderedCommGroupWithZero Γ₀) (v : Valuation ℝ Γ₀)
+(X : ℝ²) : painter Γ₀ locg v X = Rainbow.Blue → v (X 0) ≥ v (1) := by
+intro h
+simp only [painter, Fin.isValue, map_one, ge_iff_le] at h
+split_ifs at h with h1 h2
+rw [dist_negation_over_and] at h1
+rw [dist_negation_over_and] at h2
+cases' h1 with p q
+rw [not_lt] at p
+rw [v.map_one]
+apply p
+cases' h2 with m n
+rw [not_lt] at m
+rw [not_lt, ← v.map_one] at q
+exact le_trans q m
+
+rw [not_lt] at q
+contradiction
+
+
+
+
+
+lemma bounded_det (Γ₀ : Type) (locg : LinearOrderedCommGroupWithZero Γ₀) (v : Valuation ℝ Γ₀)
+(X Y Z : ℝ²) (hb: painter Γ₀ locg v X = Rainbow.Blue )(hg: painter Γ₀ locg v Y = Rainbow.Green)
+(hr: painter Γ₀ locg v Z = Rainbow.Red) :
+v (X 0 * Y 1 + X 1 * Z 0 + Y 0 * Z 1 - Y 1 * Z 0 - X 1 * Y 0 - X 0 * Z 1) ≥ 1 := by
+
+
+
+
+
+
+/-## TODO
   - Monsky's Theorem
     - proof
+inducrtive
       - Lemma 1
         - proof
       - Corollary
