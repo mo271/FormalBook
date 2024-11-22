f8365a3a2b7366003cac9675965bcbb474036ff6
dhyan-aranha <dhyan.aranha@gmail.com>
2024-11-22 23:01:04 +0100

    all sorry's resolved

--- FormalBook/Chapter_22.lean
+++ FormalBook/Chapter_22.lean
@@ -171,6 +171,8 @@ Function.Surjective (painter Γ₀ locg v ∘ T)
 -- Essentially we are just establishing bounds on valuations of the terms that appear in the
 -- definition of the area of a triangle.
 
+
+
 lemma val_bound1 (Γ₀ : Type) (locg : LinearOrderedCommGroupWithZero Γ₀) (v : Valuation ℝ Γ₀)
 (X Y: ℝ²) (hb: painter Γ₀ locg v X = Rainbow.Blue )(hg: painter Γ₀ locg v Y = Rainbow.Green) :
 v (X 0 * Y 1) ≥ 1 := by
@@ -183,6 +185,7 @@ have h3: v (X 0 * Y 1) ≥ v 1 * v 1 := by
 rw [v.map_mul]
 rw [v.map_one, one_mul, v.map_mul] at h3
 exact h3
+
 
 
 lemma val_bound2 (Γ₀ : Type) (locg : LinearOrderedCommGroupWithZero Γ₀) (v : Valuation ℝ Γ₀)
@@ -201,35 +204,113 @@ have h3 : v (X 1 * Z 0) < v (X 0) * v 1 := by
     rw [v.map_one]
     apply zero_lt_one
   exact lt_of_lt_of_le h5 h4
-have h4 : v (Y 1) ≥ v 1 := painted_green1 Γ₀ locg v Y hg
-have h5 : v (X 0) * v 1 ≤ v (X 0) * v (Y 1) :=
-  mul_le_mul' (le_refl (v (X 0))) h4
-rw [← map_mul, ← map_mul] at h5
+have h6 : v (Y 1) ≥ v 1 := painted_green1 Γ₀ locg v Y hg
+have h7 : v (X 0) * v 1 ≤ v (X 0) * v (Y 1) :=
+  mul_le_mul' (le_refl (v (X 0))) h6
+rw [← map_mul, ← map_mul] at h7
 rw [← map_mul] at h3
-exact lt_of_lt_of_le h3 h5
+exact lt_of_lt_of_le h3 h7
+
+
 
 lemma val_bound3 (Γ₀ : Type) (locg : LinearOrderedCommGroupWithZero Γ₀) (v : Valuation ℝ Γ₀)
 (X Y Z : ℝ²) (hb: painter Γ₀ locg v X = Rainbow.Blue )(hg: painter Γ₀ locg v Y = Rainbow.Green)
 (hr: painter Γ₀ locg v Z = Rainbow.Red):
-v (Y 0 * Z 1) < v (X 0 * Y 1) := by sorry
+v (Y 0 * Z 1) < v (X 0 * Y 1) := by
+
+have h1 : v (Y 0) < v (Y 1) := painted_green2  Γ₀ locg v Y hg
+have h2 : v (Z 1) < v 1 := painted_red2 Γ₀ locg v Z hr
+have h3:  v (Y 0 * Z 1) < v (Y 1) * v 1 := by
+ rw [v.map_mul]
+ apply mul_lt_mul'' h1 h2
+ apply zero_le'
+ apply zero_le'
+have h4 : v (X 0) ≥ v 1 := painted_blue1 Γ₀ locg v X hb
+have h5 : v (Y 1) * v 1 ≤ v (Y 1) * v (X 0) :=
+  mul_le_mul' (le_refl (v (Y 1))) h4
+rw [← map_mul] at h5
+rw [← map_mul] at h3
+rw [← map_mul] at h5
+have h6: v (X 0 * Y 1) = v (Y 1 * X 0) := by
+  rw [mul_comm]
+rw [h6]
+exact lt_of_lt_of_le h3 h5
+
 
 
 lemma val_bound4 (Γ₀ : Type) (locg : LinearOrderedCommGroupWithZero Γ₀) (v : Valuation ℝ Γ₀)
 (X Y Z : ℝ²) (hb: painter Γ₀ locg v X = Rainbow.Blue )(hg: painter Γ₀ locg v Y = Rainbow.Green)
 (hr: painter Γ₀ locg v Z = Rainbow.Red):
-v (Y 1 * Z 0) < v (X 0 * Y 1) := by sorry
+v (Y 1 * Z 0) < v (X 0 * Y 1) := by
+
+have h1 : v (Z 0 ) < v 1 := painted_red1 Γ₀ locg v Z hr
+have h2 : v (Z 0 * Y 1) < v 1 * v (Y 1) := by
+  rw [v.map_mul]
+  apply mul_lt_mul
+  apply h1
+  apply refl
+  have h3: v (Y 1) ≥ v 1 := painted_green1 Γ₀ locg v Y hg
+  have h4: v 1 > 0 := by
+    rw [v.map_one]
+    apply zero_lt_one
+  exact lt_of_lt_of_le h4 h3
+  apply zero_le'
+have h5: v (X 0) ≥ v 1 := painted_blue1 Γ₀ locg v X hb
+have h6:  v (Y 1) * v 1 ≤  v (Y 1) * v (X 0):=
+  mul_le_mul' (le_refl (v (Y 1))) h5
+rw [mul_comm] at h6
+have h7: v (Y 1) * v (X 0) = v (X 0 * Y 1) := by
+ rw [mul_comm, v.map_mul]
+rw [h7] at h6
+rw [mul_comm] at h2
+exact lt_of_lt_of_le h2 h6
+
+
 
 lemma val_bound5 (Γ₀ : Type) (locg : LinearOrderedCommGroupWithZero Γ₀) (v : Valuation ℝ Γ₀)
 (X Y Z : ℝ²) (hb: painter Γ₀ locg v X = Rainbow.Blue )(hg: painter Γ₀ locg v Y = Rainbow.Green)
 (hr: painter Γ₀ locg v Z = Rainbow.Red):
-v (X 1 * Y 0) < v (X 0 * Y 1) := by sorry
+v (X 1 * Y 0) < v (X 0 * Y 1) := by
+
+have h1 : v (X 1) ≤ v (X 0) := painted_blue2 Γ₀ locg v X hb
+have h2 : v (Y 0) < v (Y 1) := painted_green2 Γ₀ locg v Y hg
+have h3: v (X 1 * Y 0) < v (X 0) * v (Y 1) := by
+  rw [v.map_mul]
+  apply mul_lt_mul' h1 h2
+  apply zero_le'
+  have h4: v (Y 1) ≥ v 1 := painted_green1 Γ₀ locg v Y hg
+  have h5: v (X 0) ≥ v 1 := painted_blue1 Γ₀ locg v X hb
+  rw [v.map_one] at h5
+  have h6: v 1 > 0 := by
+    rw [v.map_one]
+    apply zero_lt_one
+  rw [← v.map_one] at h5
+  exact lt_of_lt_of_le h6 h5
+rw [← v.map_mul] at h3
+apply h3
+
+
 
 lemma val_bound6 (Γ₀ : Type) (locg : LinearOrderedCommGroupWithZero Γ₀) (v : Valuation ℝ Γ₀)
 (X Y Z : ℝ²) (hb: painter Γ₀ locg v X = Rainbow.Blue )(hg: painter Γ₀ locg v Y = Rainbow.Green)
 (hr: painter Γ₀ locg v Z = Rainbow.Red):
-v (X 0 * Z 1) < v (X 0 * Y 1) := by sorry
+v (X 0 * Z 1) < v (X 0 * Y 1) := by
 
-
+have h1 : v (Z 1) < v 1 := painted_red2 Γ₀ locg v Z hr
+have h2 : v (X 0) * v 1 ≤  v (X 0) * v (Y 1) :=
+  mul_le_mul' (le_refl (v (X 0))) (painted_green1 Γ₀ locg v Y hg)
+have h3 : v (X 0 * Z 1) < v (X 0) * v 1 := by
+  rw [v.map_mul]
+  apply mul_lt_mul' (le_refl (v (X 0))) h1
+  apply zero_le'
+  have h4: v (X 0) ≥ v 1 := painted_blue1 Γ₀ locg v X hb
+  have h5: v 1 > 0 := by
+    rw [v.map_one]
+    apply zero_lt_one
+  exact lt_of_lt_of_le h5 h4
+rw [← v.map_mul] at h3
+rw [← v.map_mul, ← v.map_mul] at h2
+exact lt_of_lt_of_le h3 h2
 
 
 
@@ -272,18 +353,104 @@ have w1 : v (X 0 * Y 1 + X 1 * Z 0 + Y 0 *Z 1) = v (X 0 * Y 1) := by
   rw [w5] at w4
   exact w4
 
-have q1 : v (X 0 * Y 1 + X 1 * Z 0 + Y 0 * Z 1 - Y 1 * Z 0) = v (X 0 * Y 1) := by sorry
-have r1 : v (X 0 * Y 1 + X 1 * Z 0 + Y 0 * Z 1 - Y 1 * Z 0 - X 1 * Y 0) = v (X 0 * Y 1) := by sorry
-have s1 : v (X 0 * Y 1 + X 1 * Z 0 + Y 0 * Z 1 - Y 1 * Z 0 - X 1 * Y 0 - X 0 * Z 1) = v (X 0 * Y 1) := by sorry
+have q1 : v (X 0 * Y 1 + X 1 * Z 0 + Y 0 * Z 1 - Y 1 * Z 0) = v (X 0 * Y 1) := by
+  have q2: v (X 0 * Y 1) ≠ v (Y 1 * Z 0) := by
+    intro h
+    have q3 := val_bound4 Γ₀ locg v X Y Z hb hg hr
+    rw [h] at q3
+    exact lt_irrefl _ q3
+  have q4: v (Y 1 * Z 0) = v (- Y 1 * Z 0) := by
+    rw [← v.map_neg, neg_mul_eq_neg_mul]
+  have q5: v (X 0 * Y 1 + X 1 * Z 0 ) ≠ v (Y 1 * Z 0) := by
+    rw [h1]
+    apply q2
+  have q6: v (X 0 * Y 1 + X 1 * Z 0 + Y 0 * Z 1) ≠ v (Y 1 * Z 0) := by
+    rw [w1]
+    apply q2
+  rw [q4] at q6
+  have q7: v (X 0 * Y 1 + X 1 * Z 0 + Y 0 * Z 1 - Y 1 * Z 0) = max (v (X 0 * Y 1 + X 1 * Z 0 + Y 0 * Z 1)) (v (-Y 1 * Z 0)) := by
+    rw [sub_eq_neg_add, add_comm, ← neg_mul]
+    exact h _ _ q6
+  have q8: max (v (X 0 * Y 1 + X 1 * Z 0 + Y 0 * Z 1)) (v (-Y 1 * Z 0)) = v (X 0 * Y 1 + X 1 * Z 0 + Y 0 * Z 1) := by
+    rw [w1]
+    rw [max_eq_left]
+    rw [← v.map_neg, neg_mul_eq_neg_mul, neg_neg]
+    exact le_of_lt (val_bound4 Γ₀ locg v X Y Z hb hg hr)
+  rw [q8] at q7
+  rw [w1] at q7
+  exact q7
 
 
+
+have r1 : v (X 0 * Y 1 + X 1 * Z 0 + Y 0 * Z 1 - Y 1 * Z 0 - X 1 * Y 0) = v (X 0 * Y 1) := by
+  have r2: v (X 0 * Y 1) ≠ v (X 1 * Y 0) := by
+    intro h
+    have r3 := val_bound5 Γ₀ locg v X Y Z hb hg hr
+    rw [h] at r3
+    exact lt_irrefl _ r3
+  have r4: v (X 0 * Y 1 + X 1 * Z 0) ≠ v (X 1 * Y 0) := by
+    rw [h1]
+    apply r2
+  have r5: v (X 0 * Y 1 + X 1 * Z 0 + Y 0 * Z 1) ≠ v (X 1 * Y 0) := by
+    rw [w1]
+    apply r2
+  have r6: v (X 0 * Y 1 + X 1 * Z 0 + Y 0 * Z 1 - Y 1 * Z 0) ≠ v (X 1 * Y 0) := by
+    rw [q1]
+    apply r2
+  have r7: v (X 0 * Y 1 + X 1 * Z 0 + Y 0 * Z 1 - Y 1 * Z 0 - X 1 * Y 0) = max (v (X 0 * Y 1 + X 1 * Z 0 + Y 0 * Z 1 - Y 1 * Z 0)) (v (-X 1 * Y 0)) := by
+    rw [sub_eq_neg_add, add_comm, ← neg_mul]
+    have neg: v (X 1 * Y 0) = v (- X 1 * Y 0) := by
+     rw [← v.map_neg, neg_mul_eq_neg_mul]
+    rw [neg] at r6
+    exact h _ _ r6
+  have r8: max (v (X 0 * Y 1 + X 1 * Z 0 + Y 0 * Z 1 - Y 1 * Z 0)) (v (-X 1 * Y 0)) = v (X 0 * Y 1 + X 1 * Z 0 + Y 0 * Z 1 - Y 1 * Z 0) := by
+    rw [q1]
+    rw [max_eq_left]
+    rw [← v.map_neg, neg_mul_eq_neg_mul, neg_neg]
+    exact le_of_lt (val_bound5 Γ₀ locg v X Y Z hb hg hr)
+  rw [r8] at r7
+  rw [q1] at r7
+  exact r7
+
+
+have s1 : v (X 0 * Y 1 + X 1 * Z 0 + Y 0 * Z 1 - Y 1 * Z 0 - X 1 * Y 0 - X 0 * Z 1) = v (X 0 * Y 1) := by
+  have s2: v (X 0 * Y 1) ≠ v (X 0 * Z 1) := by
+    intro h
+    have s3 := val_bound6 Γ₀ locg v X Y Z hb hg hr
+    rw [h] at s3
+    exact lt_irrefl _ s3
+  have s4: v (X 0 * Y 1 + X 1 * Z 0) ≠ v (X 0 * Z 1) := by
+    rw [h1]
+    apply s2
+  have s5: v (X 0 * Y 1 + X 1 * Z 0 + Y 0 * Z 1) ≠ v (X 0 * Z 1) := by
+    rw [w1]
+    apply s2
+  have s6: v (X 0 * Y 1 + X 1 * Z 0 + Y 0 * Z 1 - Y 1 * Z 0) ≠ v (X 0 * Z 1) := by
+    rw [q1]
+    apply s2
+  have s7: v (X 0 * Y 1 + X 1 * Z 0 + Y 0 * Z 1 - Y 1 * Z 0 - X 1 * Y 0) ≠ v (X 0 * Z 1) := by
+    rw [r1]
+    apply s2
+  have s8: v (X 0 * Y 1 + X 1 * Z 0 + Y 0 * Z 1 - Y 1 * Z 0 - X 1 * Y 0 - X 0 * Z 1) = max (v (X 0 * Y 1 + X 1 * Z 0 + Y 0 * Z 1 - Y 1 * Z 0 - X 1 * Y 0)) (v (-X 0 * Z 1)) := by
+    rw [sub_eq_neg_add, add_comm, ← neg_mul]
+    have neg: v (X 0 * Z 1) = v (- X 0 * Z 1) := by
+     rw [← v.map_neg, neg_mul_eq_neg_mul]
+    rw [neg] at s7
+    exact h _ _ s7
+  have s9: max (v (X 0 * Y 1 + X 1 * Z 0 + Y 0 * Z 1 - Y 1 * Z 0 - X 1 * Y 0)) (v (-X 0 * Z 1)) = v (X 0 * Y 1 + X 1 * Z 0 + Y 0 * Z 1 - Y 1 * Z 0 - X 1 * Y 0) := by
+    rw [r1]
+    rw [max_eq_left]
+    rw [← v.map_neg, neg_mul_eq_neg_mul, neg_neg]
+    exact le_of_lt (val_bound6 Γ₀ locg v X Y Z hb hg hr)
+  rw [s9] at s8
+  rw [r1] at s8
+  exact s8
 have e: v (X 0 * Y 1) ≥ 1 := val_bound1 Γ₀ locg v X Y hb hg
-
 rw [← s1] at e
 apply e
 
 
-done
+
 
 
 
