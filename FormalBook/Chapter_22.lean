f301c569bc52e7aadc7ee4bc4f5db5dfecb4acb1
JanHendriks1 <jan.hendriks03@gmail.com>
2024-11-23 21:09:45 +0100

    progress in proving valuation_ring_no_half

--- FormalBook/Chapter_22.lean
+++ FormalBook/Chapter_22.lean
@@ -65,24 +65,28 @@ lemma valuation_ring_no_half : ∃(B : ValuationSubring ℝ), (1/2) ∉ B := by
   let S := {A : Subring ℝ | (1/2) ∉ A}
   have h1 : ∀ c ⊆ S, IsChain (· ≤ ·) c → ∃ ub ∈ S, ∀ z ∈ c, z ≤ ub := by
     -- Idea: The upper bound is the union of the subrings.
+
     sorry
   have h2 := zorn_le₀ S h1
   rcases h2 with ⟨B, hl, hr⟩
   have h3 : ∀(C : Subring ℝ), (B ≤ C) ∧ (1/2) ∉ C → B = C := by
     -- Idea: This is exactly hr, so maybe change statement of
     -- inclusion_maximal_valuation to have hr as hypothesis.
-
-    sorry
+    rintro y ⟨h2, h3⟩
+    have h4 : y ∈ S := by
+      exact h3
+    have h5 : y ≤ B := hr h4 h2
+    exact LE.le.antisymm h2 h5
   have h4 := inclusion_maximal_valuation B hl h3
   cases' h4 with D hd
   use D
   -- Idea: B ∈ S so (1/2) ∉ B. D=B implies (1/2) ∉ D.
   -- Maybe again try to change statement of inclusion_maximal_valuation to:
   -- B is a valuation ring.
-
-  sorry
-
-
+  -- have B_no_half : 1/2 ∉ B := hl
+  have D_no_half : 1/2 ∉ D.toSubring := by
+    rwa[hd]
+  exact D_no_half
 
 
 lemma non_archimedean (Γ₀ : Type) [LinearOrderedCommGroupWithZero Γ₀] (K : Type) [Field K] (v : Valuation K Γ₀) :
